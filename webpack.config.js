e378ad2bb2f90efbcb1c34a92b9a3cbbaec9d149
mac <mac@macs-MacBook-Pro.local>
2020-06-21 21:22:00 +0200

    fix: typo missing ']' :(

--- webpack.config.js
+++ webpack.config.js
@@ -25,7 +25,7 @@ module.exports = {
       {
         test: /\.(png|svg|jpg|jpeg|gif|ico)$/,
         exclude: /node_modules/,
-        use: ['file-loader?name=[name].[ext]'] // ?name=[name].[ext] is only necessary to preserve the original file name
+        use: ['file-loader?name=[name].[ext]']
       }
     ]
   },
